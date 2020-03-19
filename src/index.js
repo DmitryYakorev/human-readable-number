dfedf05951068d69079ac2a2c121d44e72364ba8
urakend@mail.ru <urakend@mail.ru>
2020-03-19 19:10:17 +0300

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
+const ordinaries = [
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+];
+const tys = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety"
+];
+const teens = [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen"
+];
+
 module.exports = function toReadable (number) {
-  
+    if (number === 0) return 'zero';
+    if (number > 99) {
+        t = `${ordinaries[Math.floor(number / 100)]} hundred ${chance(number % 100)}`.trim();
+        console.log(t);
+        return t;}
+    else return chance(number);
+};
+
+function chance(number) {
+    if (number < 10) return ordinaries[number];
+    else if (number >= 10 && number < 20) return teens[number - 10];
+    else return `${tys[Math.floor(number / 10)]} ${ordinaries[number % 10]}`.trim(); 
 }
